edb438d5224a10d21e32ff080ca73644733f87d7
nanujra <renuprabha@hotmail.com>
2015-08-06 16:45:49 -0700

    FhirRestCall-Initial Checkin

---
+++ FhirRestTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+
+namespace FhirRestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            callRest();
+        }
+
+        public static void callRest()
+        {
+            var client = new RestClient("http://worden.globalgold.co.uk:8080/FHIR_a/farm/cobalt/Patient/72428");
+            var request = new RestRequest("Patient/1", Method.GET) { RequestFormat = DataFormat.Json };
+            request.AddHeader("Content-Type", "application/json; charset=UTF-8");
+            var response = client.Execute(request);
+        }
+    }
+}
